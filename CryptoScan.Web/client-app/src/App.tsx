08e343d0246c726747caa14f18925ad989004d26
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2022-04-01 09:25:02 +0200

    Added demo message queuing (#4)

--- CryptoScan.Web/client-app/src/App.tsx
+++ CryptoScan.Web/client-app/src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  let [successText, setSuccessText] = useState("");
+  
+  let onButtonClick = async () => {
+    let response = await fetch("test", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json;charset=UTF-8'
+      }, 
+      body: ""});
+    let text = await response.text();
+    setSuccessText(text);
+  }
+    
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={onButtonClick}>
+          Send message
+        </button>
+        <p>
+          {successText}
+        </p>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+# Prerequisites:
+* Docker running and switched to Linux container mode
+
+# How to run:
+1. Run `docker-compose up` in the main directory
+2. In browser, navigate to the web app via configured port (http://localhost:8002/)
+3. In browser, navigate to http://localhost:8080/ to access RabbitMQ console
