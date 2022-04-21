473c86fa3b5a86426b8be09e933d0b8ad06668bd
adobek <45313672+adobek@users.noreply.github.com>
2022-04-22 01:47:46 +0200

    Handle JSON from cryptocurrency changes queue

--- CryptoScan.Messaging.Service/src/main/java/com/cryptoscan/messaging/queue/QueueConsumer.java
+++ CryptoScan.Messaging.Service/src/main/java/com/cryptoscan/messaging/queue/QueueConsumer.java
@@ -1,25 +1,23 @@
 package com.cryptoscan.messaging.queue;
 
+import com.cryptoscan.messaging.model.message.CryptoChangesMessage;
 import com.cryptoscan.messaging.model.news.News;
 import com.cryptoscan.messaging.service.NewsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@RabbitListener(queues = "cryptoChanges")
 @RequiredArgsConstructor
 public class QueueConsumer {
 
     private final NewsService newsService;
 
-    @RabbitHandler
-    public void receive(byte[] message) {
-        String msg = new String(message);
-        List<News> news = newsService.getNewsForCurrency(msg);
+    @RabbitListener(queues = "cryptoChanges")
+    public void receive(CryptoChangesMessage message) {
+        List<News> news = newsService.getNewsForCurrency(message.getCurrency());
         news.stream().map(News::getTitle).forEach(System.out::println);
     }
 
