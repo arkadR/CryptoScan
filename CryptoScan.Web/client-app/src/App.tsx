f04ae4d0537482a00ad3869994c005ab8b8d2865
arkadR <arkadiusz.rasz@gmail.com>
2022-06-03 11:08:08 +0200

    Updated email to userId

--- CryptoScan.Web/client-app/src/App.tsx
+++ CryptoScan.Web/client-app/src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { Symbol, Subscription, TimeRange, Trend } from './Models';
 import {post, get, del, patch} from './Http';
 import './App.css';
@@ -6,20 +6,38 @@ import SubscriptionPanel from "./SubscriptionPanel";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google';
 import { Box, Button } from '@material-ui/core';
 
- const clientId = "28315789494-bmoihsf48vkjshc45dhejeg0ipimnndv.apps.googleusercontent.com";
- const quoteAsset : string = "EUR";
+const clientId = "28315789494-bmoihsf48vkjshc45dhejeg0ipimnndv.apps.googleusercontent.com";
+const quoteAsset : string = "EUR";
 
- export default function App() {
+export default function App() {
   
   let [symbols, setSymbols] = useState<Symbol[]>();
   let [subscriptions, setSubscriptions] = useState<Subscription[]>();
   let [userId, setUserId] = useState<string | undefined>(undefined);
+  let [email, setEmail] = useState<string | undefined>(undefined);
 
-  let handleLoginSuccess = (userId: string | undefined) => {
-    setUserId(userId);
-    getAvailableSymbols();
-    getSubscriptions();
+  let handleLoginSuccess = async (jwt: string | undefined) => {
+    let parsed = parseJwt(jwt!);
+    setUserId(parsed.sub);
+    setEmail(parsed.email);
   }
+  
+  useEffect(() => {
+    if (userId) {
+      getAvailableSymbols().catch(console.error);
+      getSubscriptions().catch(console.error);
+    }
+  }, [userId])
+
+  let parseJwt = (token: string) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  };
 
   let handleLogout = () => {
     googleLogout();
@@ -33,7 +51,7 @@ import { Box, Button } from '@material-ui/core';
   }
 
   let getSubscriptions = async () => {
-    let response = await get(`info/subscriptions?email=${userId}`);
+    let response = await get(`info/subscriptions?userId=${userId}`);
     let subscriptions = (await response.json()) as Subscription[];
     setSubscriptions(subscriptions);
   }
@@ -78,7 +96,7 @@ import { Box, Button } from '@material-ui/core';
                 ))}
                 {symbols?.filter(sm => !subscriptions?.find(sub => sub.symbol.baseAsset === sm.baseAsset)).map(symbol => (
                   <SubscriptionPanel 
-                    subscription={{userId: userId, symbol: symbol, timeRange: emptyTimeRange, trend: Trend.Unspecified} as Subscription}
+                    subscription={{userId: userId, email: email, symbol: symbol, timeRange: emptyTimeRange, trend: Trend.Unspecified} as Subscription}
                     symbolMode={true}
                     updateAction={update}
                     deleteAction={deleteSubscription}
@@ -92,7 +110,7 @@ import { Box, Button } from '@material-ui/core';
                 </Button>}
               </>
             : <GoogleLogin
-                onSuccess={response => handleLoginSuccess(response.clientId)} // do we need client credential? 
+                onSuccess={response => handleLoginSuccess(response.credential)} 
                 onError={() => console.log('Login Failed')}
                 auto_select
                 useOneTap
